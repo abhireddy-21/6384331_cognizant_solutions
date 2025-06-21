ef782675e6acaa89c91cabf9859621631c93eb40
AbhignaReddy <128401521+abhireddy-21@users.noreply.github.com>
2025-06-21 20:37:57 +0530

    Add files via upload

---
+++ Week_1/DesignPatternAndPrinciples/Decorator Pattern/Code/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Notifier notifier = new EmailNotifier();
+        notifier = new SMSNotifierDecorator(notifier);
+        notifier = new SlackNotifierDecorator(notifier);
+        notifier.send("Your order has been shipped!");
+    }
+}
